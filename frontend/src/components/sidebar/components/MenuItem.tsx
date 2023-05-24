c4f3153ac77866c4527de9caf62e919aa563728b
Roland Mackintosh <rolandmackintosh@outlook.com>
2023-05-24 14:46:52 -0700

    bring in sidebar

--- frontend/src/components/sidebar/components/MenuItem.tsx
+++ frontend/src/components/sidebar/components/MenuItem.tsx
@@ -5,19 +5,18 @@ interface MenuItemProps {
     text: string;
     active: boolean;
     location: string;
-    isCloseButton?: boolean;
     changeActiveItem?: () => void;
     closeSidebar?: () => void;
 }
 
-const MenuItem: React.FC<MenuItemProps> = ({ text, active, location, isCloseButton, changeActiveItem, closeSidebar }) => {
+const MenuItem: React.FC<MenuItemProps> = ({ text, active, location, changeActiveItem, closeSidebar }) => {
     return (
         <Link to={location}>
             <div
                 onClick={changeActiveItem ? changeActiveItem : closeSidebar}
                 className={`menuItemContainer flex flex-start px-6 py-4 ${active ? "activeItem" : ""}`}
             >
-                <div className={`tempReplacementForIcon w-6 mr-2`}></div>
+                <div className={`w-6 mr-2`}></div>
                 <p className={`menuItemText`}>{text}</p>
             </div>
         </Link>
