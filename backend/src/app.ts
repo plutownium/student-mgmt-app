66f56196b0d0d19d53ef1ee56767fcce68490736
Roland Mackintosh <rolandmackintosh@outlook.com>
2023-05-25 12:08:45 -0700

    update cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,7 +16,7 @@ class App {
         this.port = appInit.port;
         this.app.use(
             cors({
-                origin: ["http://localhost:3000"],
+                origin: ["http://localhost:3000", "http://localhost:5173"],
                 methods: "GET, POST, PATCH, DELETE, PUT",
                 allowedHeaders: "Content-Type, Authorization",
                 credentials: true,
