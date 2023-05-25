d44aa52e617598c7b261ce2c4ab2f30494b31611
Roland Mackintosh <rolandmackintosh@outlook.com>
2023-05-25 11:00:47 -0700

    update zod error response

--- backend/src/util/handleErrorResponse.ts
+++ backend/src/util/handleErrorResponse.ts
@@ -4,11 +4,16 @@ import { ZodError } from "zod";
 export function handleErrorResponse(response: Response, error: unknown) {
     console.log(error, "in handleErrorResponse");
     if (error instanceof ZodError) {
-        const message = JSON.parse(error.message);
+        const message = formatZodErrorLikeRegularError(error);
         return response.status(400).json({ error: { name: error.name, message } });
     } else if (error instanceof Error) {
         return response.status(400).json({ error: { name: error.name, message: error.message } });
     } else {
         return response.status(400).json(error);
     }
+}
+
+function formatZodErrorLikeRegularError(error: ZodError) {
+    const errorAsJSON = JSON.parse(error.message)[0];
+    return errorAsJSON.message;
 }
