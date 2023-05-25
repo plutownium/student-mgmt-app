d94cf4796f41143cd350ebc2fc34b18223fa2c19
Roland Mackintosh <rolandmackintosh@outlook.com>
2023-05-25 14:08:53 -0700

    cleanup unused sidebar code

--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -3,92 +3,18 @@ import { useNavigate, useLocation, Navigate } from "react-router-dom";
 import MenuItem from "./components/MenuItem";
 
 const Sidebar: React.FC<{}> = () => {
-    const location = useLocation();
-
-    function getLocation(path: string) {
-        if (path === "/dashboard") {
-            return 1;
-        } else if (path === "/search") {
-            return 2;
-        } else if (path === "/map") {
-            return 3;
-        } else if (path === "/feedback") {
-            return 4;
-        } else {
-            throw new Error("Unsupported value for path");
-        }
-    }
-
-    const [activeItem, setActiveItem] = useState(1);
-
     return (
         <div className="pt-12">
             <div className="h-full flex flex-col justify-between">
                 <div>
                     <div className="">
-                        <MenuItem
-                            changeActiveItem={() => {
-                                console.log(1);
-                                setActiveItem(1);
-                            }}
-                            location="/"
-                            text={"Home"}
-                            active={activeItem === 1}
-                        />
-                        <MenuItem
-                            changeActiveItem={() => {
-                                console.log(2);
-                                setActiveItem(2);
-                            }}
-                            location={"/students/add/new"}
-                            text={"Add New Student"}
-                            active={activeItem === 2}
-                        />
-                        <MenuItem
-                            changeActiveItem={() => {
-                                console.log(3);
-                                setActiveItem(3);
-                            }}
-                            location={"/students/list"}
-                            text={"Students List"}
-                            active={activeItem === 3}
-                        />
-                        <MenuItem
-                            changeActiveItem={() => {
-                                console.log(4);
-                                setActiveItem(4);
-                            }}
-                            location={"/courses/add/new"}
-                            text={"Add New Course"}
-                            active={activeItem === 4}
-                        />
-                        <MenuItem
-                            changeActiveItem={() => {
-                                console.log(5);
-                                setActiveItem(5);
-                            }}
-                            location={"/courses/list"}
-                            text={"Courses List"}
-                            active={activeItem === 5}
-                        />
-                        <MenuItem
-                            changeActiveItem={() => {
-                                console.log(6);
-                                setActiveItem(6);
-                            }}
-                            location={"/results/add/new"}
-                            text={"Add New Results"}
-                            active={activeItem === 6}
-                        />
-                        <MenuItem
-                            changeActiveItem={() => {
-                                console.log(7);
-                                setActiveItem(7);
-                            }}
-                            location={"/results/list"}
-                            text={"Results List"}
-                            active={activeItem === 7}
-                        />
+                        <MenuItem location="/" text={"Home"} />
+                        <MenuItem location={"/students/add/new"} text={"Add New Student"} />
+                        <MenuItem location={"/students/list"} text={"Students List"} />
+                        <MenuItem location={"/courses/add/new"} text={"Add New Course"} />
+                        <MenuItem location={"/courses/list"} text={"Courses List"} />
+                        <MenuItem location={"/results/add/new"} text={"Add New Results"} />
+                        <MenuItem location={"/results/list"} text={"Results List"} />
                     </div>
                 </div>
             </div>
--- frontend/src/components/sidebar/components/MenuItem.tsx
+++ frontend/src/components/sidebar/components/MenuItem.tsx
@@ -3,20 +3,19 @@ import { Link } from "react-router-dom";
 
 interface MenuItemProps {
     text: string;
-    active: boolean;
     location: string;
     changeActiveItem?: () => void;
     closeSidebar?: () => void;
 }
 
-const MenuItem: React.FC<MenuItemProps> = ({ text, active, location, changeActiveItem, closeSidebar }) => {
+const MenuItem: React.FC<MenuItemProps> = ({ text, location, changeActiveItem, closeSidebar }) => {
     return (
         <Link to={location}>
             <div
                 onClick={changeActiveItem ? changeActiveItem : closeSidebar}
-                className={`menuItemContainer flex flex-start pl-12 py-4 ${active ? "activeItem" : ""}`}
+                className={`menuItemContainer flex flex-start pl-12 py-4 hover:bg-slate-100`}
             >
-                <p className={`menuItemText`}>{text}</p>
+                <p className={`menuItemText text-black `}>{text}</p>
             </div>
         </Link>
     );
